03305b30941e311582e0f99c2693f52551f40af8
ksushaMarich <maricheva.ksenia@yandex.ru>
2024-11-25 15:36:04 +0300

    новый

---
+++ Films/View/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  Films
+//
+//  Created by User on 25.11.2024.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
