13ed199c6212a27eba1b639df9f98d6133920314
ksushaMarich <maricheva.ksenia@yandex.ru>
2024-11-25 14:18:41 +0300

    Initial Commit

---
+++ Films/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Films
+//
+//  Created by User on 25.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
