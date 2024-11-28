edb5dfbe2085bfb26ba808ca852777ed1957642c
ksushaMarich <maricheva.ksenia@yandex.ru>
2024-11-28 07:53:23 +0300

    делаем вью

---
+++ Films/View/BackgroundView.swift
@@ -0,0 +1,20 @@
+//
+//  BackgroundView.swift
+//  Films
+//
+//  Created by Ксюша on 28.11.2024.
+//
+
+import UIKit
+
+class BackgroundView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
