af7fc4dd5f1697fbd3930e0aa8312cc6c616eb46
ksushaMarich <maricheva.ksenia@yandex.ru>
2024-12-14 23:51:46 +0300

    Вышло добавить url компонетны но пока только к одной функции

--- Films/Network/NetworkManager.swift
+++ Films/Network/NetworkManager.swift
@@ -16,14 +16,23 @@ class NetworkManager {
     private let language = "ru-RU"
     
     private init() {}
+            
+    private func giveURLComponents(isPopular: Bool) -> URLComponents {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "api.themoviedb.org"
+        urlComponents.path = isPopular ? "/3/movie/popular" : "/3/search/movie"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "api_key", value: apiKey),
+            URLQueryItem(name: "language", value: language)]
+        return urlComponents
+    }
     
     func searchPopularMovies() async throws -> [Movie] {
         //        https://api.themoviedb.org/3/movie/popular?api_key=ВАШ_API_КЛЮЧ
         //        let path = "\(baseURL)/search/movie?api_key=\(apiKey)&query=\("Убить Билла")&language=\(language)"
         
-        let path = "\(baseURL)/movie/popular?api_key=\(apiKey)&language=\(language)"
-        
-        guard let url = URL(string: path) else { throw APIError.invalidURL }
+        guard let url = giveURLComponents(isPopular: true).url else { throw APIError.invalidURL }
         
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
@@ -36,6 +45,7 @@ class NetworkManager {
     func searchMoviesFromQuery(query: String) async throws -> [Movie] {
         
         let path = "\(baseURL)/search/movie?api_key=\(apiKey)&query=\(query)&language=\(language)"
+        print(path)
         
         guard let url = URL(string: path) else { throw APIError.invalidURL }
         
@@ -47,35 +57,34 @@ class NetworkManager {
         return []
     }
     
-    func searchMoviesFromQuery2(query: String, completion: @escaping (Result<[Movie], Error>) -> Void) {
-        
-        let path = "\(baseURL)/search/movie?api_key=\(apiKey)&query=\(query)&language=\(language)"
-        
-        guard let url = URL(string: path) else {
-            completion(.failure(NSError(domain: "Invalid URL", code: -1, userInfo: nil)))
-            return
+        func searchMoviesFromQuery2(query: String, completion: @escaping (Result<[Movie], Error>) -> Void) {
+            
+            let path = "\(baseURL)/search/movie?api_key=\(apiKey)&query=\(query)&language=\(language)"
+            
+            guard let url = URL(string: path) else {
+                completion(.failure(NSError(domain: "Invalid URL", code: -1, userInfo: nil)))
+                return
+            }
+            
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+                
+                guard let data = data else {
+                    completion(.failure(NSError(domain: "No Data", code: -1, userInfo: nil)))
+                    return
+                }
+                
+                do {
+                    let rsponse = try JSONDecoder().decode(MovieResponse.self, from: data)
+                    completion(.success(rsponse.results))
+                } catch  {
+                    completion(.failure(error))
+                }
+            }.resume()
         }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            
-            guard let data = data else {
-                completion(.failure(NSError(domain: "No Data", code: -1, userInfo: nil)))
-                return
-            }
-            
-            do {
-                let rsponse = try JSONDecoder().decode(MovieResponse.self, from: data)
-                completion(.success(rsponse.results))
-            } catch  {
-                completion(.failure(error))
-            }
-        }.resume()
-        
-    }
         
         func downloadPoster(from movie: Movie) async throws -> UIImage {
             var posterImage = UIImage(named: "PosterError")!
@@ -92,5 +101,4 @@ class NetworkManager {
             posterImage = image
             return posterImage
         }
-    
 }
